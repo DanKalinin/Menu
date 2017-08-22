f02cbf6b268680e682fb1c83efdb2d5df9477fff
Dan Kalinin <daniil551@hotmail.com>
2017-08-22 13:27:56 +0300

    Header

--- Framework/Framework/Menu.h
+++ Framework/Framework/Menu.h
@@ -6,13 +6,9 @@
 //  Copyright © 2016 Dan Kalinin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for Framework.
 FOUNDATION_EXPORT double MenuVersionNumber;
-
-//! Project version string for Framework.
 FOUNDATION_EXPORT const unsigned char MenuVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
 #import <Menu/MenuVC.h>
